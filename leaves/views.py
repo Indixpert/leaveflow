67544a63279bdbb01702c7185805237740c72571
mdmansooralam <mdmansooralam@outlook.in>
2026-03-27 10:40:22 +0530

    added print test

--- leaves/views.py
+++ leaves/views.py
@@ -8,6 +8,8 @@ import numpy as np
 from .models import LeaveRequest, LeaveType, ApprovalStep, PublicHoliday
 from .serializers import LeaveRequestSerializer, LeaveTypeSerializer, ApprovalStepSerializer
 
+print('print for testing')
+
 class LeaveTypeViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows leave types to be viewed.
